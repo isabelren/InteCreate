3d1075ff58493d78fd54ee39f395e32657172183
ISABEL <iren@seas.upenn.edu>
2015-12-08 22:42:01 -0500

    fixing s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,57 @@
 class User < ActiveRecord::Base
+  def s3_credentials
+    { bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']}
+  end
+
   has_many :friendships
   has_many :friends, :through => :friendships
 
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
- 
 
-  has_attached_file :image1, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image1, styles: { medium: "300x300>", thumb: "100x100>" },
+                             default_url: "/images/:style/missing.png",
+                             :storage => :s3,
+                             s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+                                              access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                              secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']}
   validates_attachment_content_type :image1, content_type: /\Aimage\/.*\Z/
-  has_attached_file :image2, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+  has_attached_file :image2, styles: { medium: "300x300>", thumb: "100x100>" }, 
+                              default_url: "/images/:style/missing.png",
+                              :storage => :s3,
+                              s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+                                              access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                              secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']}
   validates_attachment_content_type :image2, content_type: /\Aimage\/.*\Z/
-  has_attached_file :image3, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+
+  has_attached_file :image3, styles: { medium: "300x300>", thumb: "100x100>" }, 
+                              default_url: "/images/:style/missing.png",
+                              :storage => :s3,
+                              s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+                                              access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                              secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']}
   validates_attachment_content_type :image3, content_type: /\Aimage\/.*\Z/
-  has_attached_file :image4, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+
+  has_attached_file :image4, styles: { medium: "300x300>", thumb: "100x100>" }, 
+                              default_url: "/images/:style/missing.png",
+                              :storage => :s3,
+                              s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+                                              access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                              secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']}
   validates_attachment_content_type :image4, content_type: /\Aimage\/.*\Z/
-  has_attached_file :image5, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+
+  has_attached_file :image5, styles: { medium: "300x300>", thumb: "100x100>" }, 
+                              default_url: "/images/:style/missing.png",
+                              :storage => :s3,
+                              s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+                                              access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                              secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']}
   validates_attachment_content_type :image5, content_type: /\Aimage\/.*\Z/
 
   has_secure_password
@@ -24,6 +61,5 @@ class User < ActiveRecord::Base
   validates :password, :confirmation => true
 
   validates_length_of :password, :in => 6..20, :on => :create
-
 end
 
